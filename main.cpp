6071990dd55c1881d2649f6d8a4d52cc42601bbb
kaetahr <a00825945@itesm.mx>
2019-03-04 21:34:59 -0600

    now gets materias

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <string>
 using namespace std;
 
 #include "Materia.h"
@@ -7,19 +10,125 @@ using namespace std;
 #include "Fecha.h"
 #include "EjemploVideo.h"
 
+/* getMatoAut
+ * Obtiene input the un archivo cuyo nombre es dado y lo lee en el formata ID Nombre
+ * Inputs: El nombre del archivo
+ * Outputs: Escribe a un arreglo de objetos (sobrecargada a para soportar los diferentes tipos), el arreglo donde se guardaran los datos, y una entero para guardar el numero de este.
+ * Regresa verdadero si encuentra el archivo, falso si no
+ */
+bool getData(string fileName, Materia materias[5], short int &cant)
+{ 
+	cant = 0;
+	ifstream inFile;
+	inFile.open(fileName.c_str());
+	if(!inFile.is_open())
+	{ 
+		cout << "No se encontró el archivo." << endl;
+		return false;
+	}
+	
+	string data;
+	while(getline(inFile, data))
+	{ 
+		short int pos = data.find(" ");
+		materias[cant].setID(strtol(data.substr(0,pos).c_str(), NULL, 10));
+		cout << materias[cant].getID() << endl;
+		materias[cant++].setNombre(data.substr(pos + 1));
+	}
+
+	return true;
+}
+
+bool getData(string fileName, Tema temas[5], short int &cant)
+{ 
+	cant = 0;
+	ifstream inFile;
+	inFile.open(fileName.c_str());
+	if(!inFile.is_open())
+	{ 
+		cout << "No se encontró el archivo." << endl;
+		return false;
+	}
+}
+
+bool getData(string fileName, Autor autores[10], short int &cant)
+{ 
+	cant = 0;
+	ifstream inFile;
+	inFile.open(fileName.c_str());
+	if(!inFile.is_open())
+	{ 
+		cout << "No se encontró el archivo." << endl;
+		return false;
+	}
+}
+
+bool getData(string fileName, EjemploVideo videos[20], short int &cant)
+{ 
+	cant = 0;
+	ifstream inFile;
+	inFile.open(fileName.c_str());
+	if(!inFile.is_open())
+	{ 
+		cout << "No se encontró el archivo." << endl;
+		return false;
+	}
+}
+/* main
+ * Llama todas las otras funciones
+ * Input: NONE
+ * Output: 0
+ */
 int main()
 { 
-	Materia mTest;
-	Tema tTest;
-	Autor aTest;
-	Fecha fTest;
-	EjemploVideo eTest;
+	//Variables
+	Materia materias[5];
+	short int cantMaterias;
+	Tema temas[10];
+	short int cantTemas;
+	Autor autores[10];
+	short int cantAutores;
+	EjemploVideo videos[20];
+	short int cantVideos;
 
-	cout << mTest.getID();
-	cout << tTest.getIDTema();
-	cout << aTest.getIDAutor();
-	cout << fTest.getDD();
-	cout << eTest.getIDVideo();
+	//Input
+	char i = 0;
+	while (i < 4)
+	{ 
+		bool exists;
+		string fileName;
+		switch(i)
+		{ 
+			case 0:
+				cout << "¿Cuál es el nombre del archivo de materias" << endl;
+				cin >> fileName;
+				exists = getData(fileName, materias, cantMaterias);
+				break;
+			case 1:
+				cout << "¿Cuál es el nombre del archivo de temas?" << endl;
+				cin >> fileName;
+				exists = getData(fileName, temas, cantTemas);
+				break;
+			case 2:
+				cout << "¿Cuál es el nombre del archivo de autores?" << endl;
+				cin >> fileName;
+				exists = getData(fileName, temas, cantTemas);
+				break;
+			case 3:
+				cout << "¿Cuál es el nombre del archivo de videos?" << endl;
+				cin >> fileName;
+				exists = getData(fileName, temas, cantTemas);
+				break;
 
+		}
+		if (!exists)
+		{ 
+			cout << "No se encontró el archivo" << endl;
+			return 0;
+		}
+		i++;
+	}
+	
+	menu();
 	return 0;
 }
