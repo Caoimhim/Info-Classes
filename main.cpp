69e0d210126807f788a60b867fc59c5ab15b03b1
kaetahr <a00825945@itesm.mx>
2019-03-05 18:42:11 -0600

    fixed reading videos

--- main.cpp
+++ main.cpp
@@ -10,7 +10,37 @@ using namespace std;
 #include "Fecha.h"
 #include "EjemploVideo.h"
 
-/* getMatoAut
+/* checkExists
+ * Revisa si existe el id de una objeto en un arreglo dado
+ * Inputs: El arreglo de temas a revisar
+ * Outputs: Verdadero o Falso
+ */
+bool checkExists(int tema, Tema temas[30], short int cant)
+{ 
+	for(short int i = 0; i < cant; i++)
+	{ 
+		if(tema == temas[i].getIDTema())
+		{ 
+			return true;
+		}
+	}
+	return false;
+}
+
+bool checkExists(int autor, Autor autores[30], short int cant)
+{ 
+	for(short int i = 0; i < cant; i++)
+	{ 
+		if(autor == autores[i].getIDAutor())
+		{ 
+			return true;
+		}
+	}
+	return false;
+}
+
+
+/* getData
  * Obtiene input the un archivo cuyo nombre es dado y lo lee en el formata ID Nombre
  * Inputs: El nombre del archivo
  * Outputs: Escribe a un arreglo de objetos (sobrecargada a para soportar los diferentes tipos), el arreglo donde se guardaran los datos, y una entero para guardar el numero de este.
@@ -79,7 +109,7 @@ bool getData(string fileName, Autor autores[10], short int &cant)
 	return true;
 }
 
-bool getData(string fileName, EjemploVideo videos[20], short int &cant)
+bool getData(string fileName, EjemploVideo videos[20], short int &cant, Tema temas[30], short int cantTemas, Autor autores[30], short int cantAutores)
 { 
 	cant = 0;
 	ifstream inFile;
@@ -88,40 +118,82 @@ bool getData(string fileName, EjemploVideo videos[20], short int &cant)
 	{ 
 		return false;
 	}
-	int data;
-	while(inFile >>  data)
+	string data; 
+	while(getline(inFile, data))
 	{ 
-		videos[cant].setIDVideo(data);
+		string buffer;
 
-		string nombre;
-		inFile >> nombre;
-		videos[cant].setNombre(nombre);
+		short int pos = data.find(" ");
+		int idVideo = strtol(data.substr(0,pos).c_str(), NULL, 10);
 
-		inFile >> data;
-		videos[cant].setIDTema(data);
+		short int pos2 = data.find(" ", pos+1);
+		string nombre = data.substr(pos+1, pos2 - pos -1);
 
-		int d, m, a;
-		inFile >> d;
-		inFile >> m;
-		inFile >> a;
-
-		Fecha fechaElab;
-		fechaElab.setFecha(d, m, a);
-		videos[cant].setfechaElaboracion(fechaElab);
-
-		int autorCount;
-		inFile >> autorCount;
-		while (autorCount > 0)
+		pos = data.find(" ", pos2+1);
+		buffer = data.substr(pos2+1, pos);
+		int idTema = strtol(buffer.c_str(), NULL, 10);
+		if (checkExists(idTema, temas, cantTemas))
 		{ 
-			inFile >> data;
-			videos[cant].agregaAutor(data);
-			autorCount--;
-		}
-		cant++;
+	
+			pos2 = data.find(" ", pos+1);
+			buffer = data.substr(pos+1, pos2);
+			int d = strtol(buffer.c_str(), NULL, 10);
+	
+			pos = data.find(" ", pos2+1);
+			buffer = data.substr(pos2+1, pos);
+			int m = strtol(buffer.c_str(), NULL, 10);
+	
+			pos2 = data.find(" ", pos+1);
+			buffer = data.substr(pos+1, pos2);
+			int a = strtol(buffer.c_str(), NULL, 10);
+	
+			pos = data.find(" ", pos2+1);
+			buffer = data.substr(pos2+1, pos);
+			int cantAutores = strtol(buffer.c_str(), NULL, 10);
+	
+			data = data.substr(pos+1);
+			int inAutores[10];
+			int idAutor;
+			bool exists = true; 
+			for (unsigned char i = 0; i < cantAutores && exists; i++)
+			{ 
+				pos = data.find(" ");
+				idAutor = strtol(data.substr(0,pos).c_str(), NULL, 10);
+	
+				if(!checkExists(idAutor, autores, cantAutores))	
+				{ 
+					exists = false;	
+				}
+				inAutores[i] = idAutor;
+			}
+			if (!exists)
+			{ 
+				cout << "¡Advertencia! No se encontró el autor con la ID " << idAutor << endl;
+			}
+			else
+			{ 
+				videos[cant].setIDVideo(idVideo);
+				videos[cant].setNombre(nombre);
+				videos[cant].setIDTema(idTema);
+				Fecha fechaElab;
+				fechaElab.setFecha(d, m, a);
+				videos[cant].setfechaElaboracion(fechaElab);
 
+				for (unsigned char i = 0; i < cantAutores; i++)
+				{ 
+					videos[cant].agregaAutor(inAutores[i]);
+				}
+				cant++;
+			}
+		}
+		else
+		{ 
+			cout << "¡Advertencia! No se encontró el tema con la ID " << idTema << endl;
+		}
 	}
 	return true;
 }
+
 /* main
  * Llama todas las otras funciones
  * Input: NONE
@@ -160,12 +232,12 @@ int main()
 			case 2:
 				cout << "¿Cuál es el nombre del archivo de autores?" << endl;
 				cin >> fileName;
-				exists = getData(fileName, temas, cantTemas);
+				exists = getData(fileName, autores, cantAutores);
 				break;
 			case 3:
 				cout << "¿Cuál es el nombre del archivo de videos?" << endl;
 				cin >> fileName;
-				exists = getData(fileName, temas, cantTemas);
+				exists = getData(fileName, videos, cantVideos, temas, cantTemas, autores, cantAutores);
 				break;
 
 		}
